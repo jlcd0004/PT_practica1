a527edb12f109c2a8ff7b9bc4af3f87ab474d795
Jose Luis Cabeza Diaz <jlcd0004@red.ujaen.es>
2015-10-18 17:27:51 +0200

    Código comentado.
    Añadidos comentarios al código.

--- cliente/cliente.c
+++ cliente/cliente.c
@@ -32,7 +32,8 @@ int main(int *argc, char *argv[])
 	int recibidos=0,enviados=0;
 	int estado=S_HELO;
 	char option;
-	int com=0;
+	int com = 0; 
+
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	int err;
@@ -53,33 +54,64 @@ int main(int *argc, char *argv[])
 	}
 	//Fin: Inicialización Windows sockets
 
+	/*Creación del Socket TCP del servidor.*/
 	do{
-		sockfd=socket(AF_INET,SOCK_STREAM,0);
+		sockfd=socket(AF_INET,SOCK_STREAM,0); 
+	/***************************************************************************************************************************
+	Aquí se crea el socket al utilizar la función socket, la cual es la primitiva que nos permite crear un descriptor de socket.
+	La función socket se iguala a la variable sockfd, la cual recibirá el valor de retorno de la función socket, si no ocurre 
+	ningún error, la función devuelve un descriptor que referencia un nuevo socket (valor int). En otro caso devuelve el valor -1
+	(macro INVALID_SOCKET), el código de error específico se puede obtener llamando a WSAGetLastError (o GetLastError). 
 
-		if(sockfd==INVALID_SOCKET)
-		{
+	En caso de no tener un fallo, este valor entero no negativo, será el que llamaremos un descriptor socket, y es pasado a otras
+	funciones API para identificar la abstración socket sobre la cual se va a llevar a cabo la operación.
+
+	int socket(int af, int type, int protocol); 
+	af: Familia de direcciones, en este caso AF_INET -> IPv4.
+	type: Especificación del tipo del nuevo socket, en este caso SOCK_STREAM -> usar conexiones TCP. 
+	protocol: Indica el procolo dentro de la familia elegida -> Por defecto usamos 0, protocolo por defecto de la familia.
+	****************************************************************************************************************************/
+		if(sockfd==INVALID_SOCKET) //En caso de que la función socket = sockfd me devuelva un error (INVALID_SOCKET), imprimo error al 
+		{                          //usuario a la hora de crear el Socket. 
 			printf("CLIENTE> ERROR AL CREAR SOCKET\r\n");
 			exit(-1);
 		}
-		else
+		else //En caso de que no haya error, se imprime al cliente que se ha creado el SOCKET correctamente. 
 		{
 			printf("CLIENTE> SOCKET CREADO CORRECTAMENTE\r\n");
 
-		
+		    //Pasamos a pedirle al dirección IP de destino al usuario. 
 			printf("CLIENTE> Introduzca la IP destino (pulsar enter para IP por defecto): ");
-			gets(ipdest);
-
+			gets(ipdest); //gets me toma lo que imprimo en la ventana CMD. 
+			/*strcmp, me compara los dos string que se le introducen como argumentos, y en caso de ser iguales, diría que ipdest es 0 
+			y por tanto el usuario quiere usar la IP por defecto.*/
 			if(strcmp(ipdest,"")==0)
-				strcpy(ipdest,default_ip);
+				strcpy(ipdest,default_ip); //En caso de cumplirse el if, me copia en ipdest la dirección default_ip. 
 
-
-			server_in.sin_family=AF_INET;
-			server_in.sin_port=htons(TCP_SERVICE_PORT);
-			server_in.sin_addr.s_addr=inet_addr(ipdest);
-			
-			estado=S_HELO;
+			//Estructuras de direcciones de IPv4.
+			server_in.sin_family=AF_INET;                //Campo de la estructura sockaddr_in para el Protocolo de Internet. 
+			server_in.sin_port=htons(TCP_SERVICE_PORT);  //Campo de la estructura sockaddr_in para introducir en el que escucha el servidor, el puerto que está ya definido como #define TCP_SERVICE_PORT 6000.
+			server_in.sin_addr.s_addr=inet_addr(ipdest); //Campo de la estructura sockaddr_in para introducir la dirección de Internet (32 bits).
+			//inet_addr: function converts a string containing an IPv4 dotted-decimal address into a proper address.
+			//htons: function makes sure that numbers are stored in memory in network byte order
+			estado=S_HELO;//Inicializo el estado en S_HELO, estado 0 definido en la cabecera protocol.h. 
 		
-			// establece la conexion de transporte
+			/***************************************************************************************************************************
+			Un socket TCP debe ser conectado a otro socket antes de que cualquier dato pueda ser enviado a través de él. El cliente 
+			debe iniciar la conexión mientras el servidor espera pasívamente por clientes que se conecten a él. Para establecer la 
+			conexión con el servidor, utilizamos la función connect(). 
+
+            int connect(int socket, struct sockaddr *foreignAddress, unsigned int addressLength)
+
+			socket -> Es el descriptor creado para un determinado socket.
+
+			foreignAddress -> Declarado como un puntero hacia un sockaddr será siempre un puntero a un sockaddr_in conteniendo la 
+                              dirección de internet y el puerto del servidor. 
+
+			addressLength -> especifica la longitud de la dirección de la estructura
+           
+		    Cuando connect() vuelve exitósamente, el socket es conectado y la comunicación puede proceder con llamadas a send() y recv().
+			****************************************************************************************************************************/
 			if(connect(sockfd,(struct sockaddr*)&server_in,sizeof(server_in))==0)
 			{
 				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
@@ -95,93 +127,113 @@ int main(int *argc, char *argv[])
 					case S_USER:
 						// establece la conexion de aplicacion 
 						printf("CLIENTE> Introduzca el usuario (enter para salir): ");
-						gets(input);
-						if(strlen(input)==0)
+						gets(input);//Tomo lo que introduzca el usuario. 
+						if(strlen(input)==0)//La misión de la función strlen es contar el nº de carácteres de una cadena.
 						{
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);//Imprimir de forma segura. 
+							estado=S_QUIT; //Si pulso enter, se acaba la conexión al pasarme al estado S_QUIT. 
 						}
 						else
 
 						sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",SC,input,CRLF);
 						break;
+						//Autentificación del usuario. 
 					case S_PASS:
 						printf("CLIENTE> Introduzca la clave (enter para salir): ");
-						gets(input);
-						if(strlen(input)==0)
+						gets(input);//Tomo la cadena que introduzca el usuario. 
+						if(strlen(input)==0)//En caso de ser enter, salgo de la conexión por estado S_QUIT.
 						{
 							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
 							estado=S_QUIT;
 						}
-						else
+						else//De otra forma se imprime la contraseña y se transmite. 
 							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",PW,input,CRLF);
 						break;
+						//Una vez establecida la conexión, y autentificado el usuario se pasa a transmitir los datos.
 					case S_DATA:
 						printf("CLIENTE> Introduzca dos numeros enteros de maximo 4 digitos precedidos de 'sum' (enter o QUIT para salir): ");
 						gets(input);
 						if(strlen(input)==0)
 						{
 							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
+							estado=S_QUIT;//En caso de introducir enter, cierro la conexión al estar en el estado S_QUIT:
 						}
-						else
+						else//En otro caso se imprime lo que el usuario escriba por teclado. 
 							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",input,CRLF);
 						break;
 				 
 				
 					}
-					//Envio
-					if(estado!=S_HELO)
-					// Ejercicio: Comprobar el estado de envio
-						enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
-					if(com==0){//Con el entero 'com' compruebo si estoy en la primera iteración o en el resto.
+            /***************************************************************************************************************************
+		    Envio de mensajes TCP.
+			int send(int socket, const void *msg, unsigned int msg_len, int flags);
+
+			socket -> Es el descriptor creado para un determinado socket.
+
+			msg    -> Apunta al mensaje para enviar. 
+
+			msgLength -> Es la longitud (bytes) del mensaje.
+			
+			send me devuelve el número de bytes si no hay error, si me envía un -1 es error y un 0 es que se libera la conexión de forma 
+			acordada o por algún problema entre servidor y cliente. 
+			****************************************************************************************************************************/
+					if(estado!=S_HELO)//En todo momento que el estado no sea S_HELO, se ejecutan las funciones recv y send. Las cuales
+						              //son las que nos permitirán transmitir y recibir información del servidor. 
+					enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
+					 if(com==0){//Con el entero 'com' compruebo si estoy en la primera iteración o en el resto.
 						com=1;
-						if(enviados<0) {
-							DWORD error=GetLastError();
-							printf("CLIENTE> Error %d en el envio de datos\r\n",error);
-							estado=S_QUIT;}
+						if(enviados<0) {//En el caso de que la función send devuelva un valor negativo informo al usuario del error. 
+							DWORD error=GetLastError();//Tomo el tipo de error producido. 
+							printf("CLIENTE> Error %d en el envio de datos\r\n",error);//Imprimo el error al usuario. 
+							estado=S_QUIT;//Cierro la conexión. 
+						}
 					}else{
-							if(enviados<=0)
+							if(enviados<=0)//En el caso de que send me devuelva un valor negativo/cero.
 					{
-						DWORD error=GetLastError();
-						if(enviados<0)
+						DWORD error=GetLastError();//Tomo el error que ha tenido lugar. 
+						if(enviados<0)//En caso de que se termine la conexión aviso al usuario.
 						{
 							printf("CLIENTE> Error %d en envio de datos\r\n",error);
 							estado=S_QUIT;
 						}
 						else
 						{
-							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
-							estado=S_QUIT;}
-						
-					
+							printf("CLIENTE> Conexion con el servidor cerrada\r\n");
+							estado=S_QUIT;
+						 }
 						}
-					}
+					   }
+            /********************************************************************************************************************************
+		    Envio de mensajes TCP.
+			int recv(int socket, void *rcvBuffer, unsigned int bufferLength, int flags);
 
-					
-					
-					//Recibo
+			socket       -> Es el descriptor creado para un determinado socket.
+
+			rcvBuffer    -> Apunta al buffer --que es, un área en memoria como un carácter array-- donde los datos recibidos serán colocados.
+
+			bufferLength -> Nos da la longitud del buffer, esta longitud es el número máximo de bytes que pueden ser recibidos de una vez.
+			
+			recv me devuelve el número de bytes si no hay error, si me envía un -1 es error y un 0 es que se libera la conexión de forma 
+			acordada o por algún problema entre cliente y servidor. 
+			*******************************************************************************************************************************/
 					recibidos=recv(sockfd,buffer_in,512,0);
-					
-					if(recibidos<=0)
+					if(recibidos<=0)//Compruebo lo que me devuelve la función recv, y en caso de devolver valores de error se lo hago saber al usuario y libero la conexión.
 					{
 						DWORD error=GetLastError();
 						if(recibidos<0)
 						{
-							printf("CLIENTE> Error %d en la recepción de datos\r\n",error);
+							printf("CLIENTE> Error %d en la recepcion de datos\r\n",error);
 							estado=S_QUIT;
 						}
 						else
 						{
-							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
-							estado=S_QUIT;
-						
-					
+							printf("CLIENTE> Conexion con el servidor cerrada\r\n");
+							estado=S_QUIT;											
 						}
-					}else
+					}else //En caso de que recv no me devuelva un valor de error, imprimo los datos que reciba el cliente del servidor y son guardados en el buffer_in.
 					{
-						buffer_in[recibidos]=0x00;
-						printf(buffer_in);
+						buffer_in[recibidos]=0x00;//Los arrays en C deben acabar en 0x00.
+						printf(buffer_in);//Imprimo lo que recibo. 
 						if(estado!=S_DATA && strncmp(buffer_in,OK,2)==0) 
 							estado++;  
 					}
@@ -193,14 +245,14 @@ int main(int *argc, char *argv[])
 			}
 			else
 			{
-				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
+				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);//Error al tratar de conectar con el servidor. 
 			}		
 			// fin de la conexion de transporte
-			closesocket(sockfd);
+			closesocket(sockfd);//Comando para cerrar la conexión int closesocket(int socket);
 			
 		}	
 		printf("-----------------------\r\n\r\nCLIENTE> Volver a conectar (S/N)\r\n");
-		option=_getche();
+		option=_getche();//lee un solo carácter de la consola con repetición, es decir, el carácter se muestra en la consola.
 
 	}while(option!='n' && option!='N');
 
--- servidor/servidor.c
+++ servidor/servidor.c
@@ -30,12 +30,13 @@ main()
 	WSADATA wsaData;
 	SOCKET sockfd,nuevosockfd;
 	struct sockaddr_in  local_addr,remote_addr;
-	char buffer_out[1024],buffer_in[1024], cmd[50], usr[10], pas[10], SUM[10];
+	char buffer_out[1024],buffer_in[1024], cmd[10], usr[10], pas[10], SUM[10]; //Se añade un array para el comando sum. 
 	int err,tamanio;
 	int fin=0, fin_conexion=0;
 	int recibidos=0,enviados=0;
 	int estado=0;
-	int NUM1,NUM2,SUMA;
+	int NUM1,NUM2,SUMA; //Variables para los números enteros introducidos y el resultado de la suma. 
+
 	/** INICIALIZACION DE BIBLIOTECA WINSOCK2 **
 	 ** OJO!: SOLO WINDOWS                    **/
 	wVersionRequested=MAKEWORD(1,1);
@@ -50,23 +51,66 @@ main()
 	/** FIN INICIALIZACION DE BIBLIOTECA WINSOCK2 **/
 
 
-	sockfd=socket(AF_INET,SOCK_STREAM,0);//Creación del socket
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	/***************************************************************************************************************************
+	Aquí se crea el socket al utilizar la función socket, la cual es la primitiva que nos permite crear un descriptor de socket.
+	La función socket se iguala a la variable sockfd, la cual recibirá el valor de retorno de la función socket, si no ocurre 
+	ningún error, la función devuelve un descriptor que referencia un nuevo socket (valor int). En otro caso devuelve el valor -1
+	(macro INVALID_SOCKET), el código de error específico se puede obtener llamando a WSAGetLastError (o GetLastError). 
 
+	En caso de no tener un fallo, este valor entero no negativo, será el que llamaremos un descriptor socket, y es pasado a otras
+	funciones API para identificar la abstración socket sobre la cual se va a llevar a cabo la operación.
+
+	int socket(int af, int type, int protocol); 
+	af: Familia de direcciones, en este caso AF_INET -> IPv4.
+	type: Especificación del tipo del nuevo socket, en este caso SOCK_STREAM -> usar conexiones TCP. 
+	protocol: Indica el procolo dentro de la familia elegida -> Por defecto usamos 0, protocolo por defecto de la familia.
+	****************************************************************************************************************************/
+	//En caso de que la función socket = sockfd me devuelva un error (INVALID_SOCKET), imprimo error al 
+	//usuario a la hora de crear el Socket. 
 	if(sockfd==INVALID_SOCKET)	{
 		return(-3);
 	}
-	else {
-		local_addr.sin_family		=AF_INET;			// Familia de protocolos de Internet
-		local_addr.sin_port			=htons(TCP_SERVICE_PORT);	// Puerto del servidor
-		local_addr.sin_addr.s_addr	=htonl(INADDR_ANY);	// Direccion IP del servidor Any cualquier disponible
-													// Cambiar para que conincida con la del host
+	else { //En caso de que no haya error.
+		//Estructuras de direcciones de IPv4.
+		local_addr.sin_family		=AF_INET;			        //Campo de la estructura sockaddr_in para el Protocolo de Internet. 
+		local_addr.sin_port			=htons(TCP_SERVICE_PORT);	//Campo de la estructura sockaddr_in para introducir en el que escucha el servidor, el puerto que está ya definido como #define TCP_SERVICE_PORT 6000.
+		local_addr.sin_addr.s_addr	=htonl(INADDR_ANY);	        // Direccion IP del servidor Any cualquier disponible
+													            // Cambiar para que conincida con la del host
 	}
 	
 	// Enlace el socket a la direccion local (IP y puerto)
 	if(bind(sockfd,(struct sockaddr*)&local_addr,sizeof(local_addr))<0)
 		return(-4);
-	
-	//Se prepara el socket para recibir conexiones y se establece el tamaño de cola de espera
+/*********************************************************************************************
+int bind(int socket, struct sockaddr *localAddress, unsigned int addressLength)
+
+Parámetros:
+
+socket        -> Descriptor del socket que fue creado con la función socket(). 
+localAddress  -> El parámetro de la dirección es declarado como un puntero a sockaddr, pero para aplicaciones TCP/IP, 
+                 será realmente un puntero a sockaddr_in conteniendo la dirección de Internet de la interfaz local y 
+				 el puerto en el que escuchar.
+addressLength -> Loongitud de la estructura de dirección.
+
+bind() devuelve 0 para un éxito y -1 para un fallo. Si es un éxito, el socket identificado por el descriptor dado (y no otro)
+es asociado con la dirección IP y el puerto dados. La dirección de Internet puede ser colocada al valor especial INADDR_ANY,
+lo que significa que las conexiones al puerto especificado serán dirigidas a este socket, sin importar a que direcciones de 
+internet son enviadas;  
+*********************************************************************************************/
+
+/*********************************************************************************************
+int listen(int socket, int queueLimit)
+
+Parámetros:
+
+socket      -> Descriptor del socket que fue creado con la función socket(). 
+queueLimit  -> Especifica un máximo del número de conexiones entrantes que puede esperar en cualquier momento. 
+
+Las peticiones entrantes de conexión TCP serán manejadas y entonces colocadas en cola para ser aceptadas por el programa.  
+
+listen() devuelve 0 para el éxito y -1 para un fallo. 
+*********************************************************************************************/
 	if(listen(sockfd,5)!=0)
 		return (-6);
 	
@@ -75,7 +119,24 @@ main()
 	do
 	{
 		printf ("SERVIDOR> ESPERANDO NUEVA CONEXION DE TRANSPORTE\r\n");
-		
+/*********************************************************************************************
+Ahora el socket que ha sido atado a un puerto y marcado para "escuchar" no será usado realmente
+para enviar y recibir. En su lugar, es usado como una forma de obtener nuevos sockets, para 
+cada conexión de cliente; el servidor envía y recibe en estos nuevos sockets. El servidor 
+obtener un socket para una conexión entrante de cliente al llamar a la función accept(). 
+
+int accept(int socket, struct sockaddr *clientAddress, unsigned int *addressLength)
+
+Parámetros:
+
+socket        -> Descriptor del socket que fue creado con la función socket(). 
+clientAddress -> Llena la estructura sockaddr apuntando por clientAddress, con la dirección del cliente.
+addressLength -> Especifica el tamaño máximo de la dirección clientAddress. 
+
+En un éxito, accept() devuelve un descriptor para un nuevo socket que está conectado al cliente. Si 
+es un fallo, devuelve -1. El servidor se comunica con el cliente usando send() y recv(); Cuando la 
+comunicación se completa, esta se termina con la función close(). 
+*********************************************************************************************/
 		nuevosockfd=accept(sockfd,(struct sockaddr*)&remote_addr,&tamanio);
 
 		if(nuevosockfd==INVALID_SOCKET) {
@@ -91,26 +152,23 @@ main()
 		
 		enviados=send(nuevosockfd,buffer_out,(int)strlen(buffer_out),0);
 		//TODO Comprobar error de envío
-		if(enviados<=0)
+				if(enviados<=0)                     //En caso de que la función send me devuelva un valor entero negativo o cero, informo al cliente del error. 
 					{
-						DWORD error=GetLastError();
-						if(enviados<0)
+						DWORD error=GetLastError(); //Tomo el error que se ha producido. 
+						if(enviados<0)              //En caso de que me devuelva un valor entero negativo, aviso al cliente del error. 
 						{
 							printf("CLIENTE> Error %d en el envio de datos\r\n",error);
-							estado=S_USER;
+							estado=S_USER;          //Vuelvo al estado S_USER. 
 							fin_conexion = 1;
 						}
-						else
+						else //En otro caso. 
 						{
-							printf("CLIENTE> Conexión con el cliente cerrada\r\n");
-							estado=S_USER;
-							fin_conexion = 1;}
-						
-					
+							printf("CLIENTE> Conexión con el cliente cerrada\r\n"); //Se cierra la conexión con el usuario. 
+							estado=S_USER;                                          //Volvemos al estado S_USER. 
+							fin_conexion = 1;
+						}											
 		}
-		
- 
-		//Se reestablece el estado inicial
+        //Se reestablece el estado inicial
 		estado = S_USER;
 		fin_conexion = 0;
 
@@ -120,31 +178,30 @@ main()
 			//Se espera un comando del cliente
 			recibidos = recv(nuevosockfd,buffer_in,1023,0);
 			//TODO Comprobar posible error de recepción
-			if(recibidos<=0)
+			if(recibidos<=0)                           //En caso de que la función recv me devuelva un valor entero negativo o cero, informo al cliente del error. 
 					{
-						DWORD error=GetLastError();
-						if(recibidos<0)
+						DWORD error=GetLastError();    //Tomo el error que se ha producido. 
+						if(recibidos<0)                //En caso de que me devuelva un valor entero negativo, aviso al cliente del error. 
 						{
 							printf("CLIENTE> Error %d en la recepcion de datos\r\n",error);
 							estado=S_USER;
 							fin_conexion = 1;
 						}
-						else
+						else //En otro caso, se cierra la conexión y se vuelve al estado S_USER. 
 						{
 							printf("CLIENTE> Conexión con el cliente cerrada\r\n");
 							estado=S_USER;
-							fin_conexion = 1;}
-						
-					
+							fin_conexion = 1;
+						}											
 			}
 			
-			buffer_in[recibidos] = 0x00;
-			printf ("SERVIDOR [bytes recibidos]> %d\r\nSERVIDOR [datos recibidos]>%s", recibidos, buffer_in);
-			
+			buffer_in[recibidos] = 0x00; //Tomo el array de los datos recibidos por el servidor y en C los arrays finalizan con el byte 0000 0000. 
+			printf ("SERVIDOR [bytes recibidos]> %d\r\nSERVIDOR [datos recibidos]>%s", recibidos, buffer_in); //Imprimo el array con los datos recibidos por el servidor.
+			//Comienzo la máquina de estados finita. 
 			switch (estado)
 			{
 				case S_USER:    /*****************************************/
-					strncpy_s ( cmd, sizeof(cmd),buffer_in, 4);
+					strncpy_s ( cmd, sizeof(cmd),buffer_in, 4);//Copia caracteres de una cadena en otra.
 					cmd[4]=0x00; // en C los arrays finalizan con el byte 0000 0000
 
 					if ( strcmp(cmd,SC)==0 ) // si recibido es solicitud de conexion de aplicacion
@@ -188,12 +245,12 @@ main()
 						}
 						else
 						{
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s Autenticación errónea%s",ER,CRLF);
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Autenticación erronea%s",ER,CRLF);
 						}
 					} else
 					if ( strcmp(cmd,SD)==0 )
 					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
+						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexion%s", OK,CRLF);
 						fin_conexion=1;
 					}
 					else
@@ -207,13 +264,13 @@ main()
 					buffer_in[recibidos] = 0x00;
 					
 					strncpy_s(cmd,sizeof(cmd), buffer_in, 4);
-					sscanf(buffer_in,"%s%d%d",&SUM,&NUM1,&NUM2);
+					sscanf(buffer_in,"%s%d%d",&SUM,&NUM1,&NUM2);//Extraigo del mensaje el comando y los dos números enteros.
 
 					printf ("SERVIDOR [Comando]>%s\r\n",SUM);
 					
 					if ( strcmp(cmd,SD)==0 )
 					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
+						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexion%s", OK,CRLF);
 						fin_conexion=1;
 					}
 					else if (strcmp(cmd,SD2)==0)
@@ -223,19 +280,18 @@ main()
 						fin=1;
 					}
 					else
-					{
-						
-						if(strcmp(SUM,"sum")==0 && NUM1<9999 && NUM2<9999){
-							SUMA=NUM1+NUM2;
-							printf( "ok NUM1:%d  NUM2:%d SUM:%d %s",NUM1,NUM2,SUMA,CRLF);
-							sprintf_s(buffer_out, sizeof(buffer_out), "ok %d %s",SUMA,CRLF);
+			        {   
+					if(strcmp(SUM,"sum")==0 && NUM1<9999 && NUM2<9999){ //Compruebo que el comando es sum, y que los números son de 4 cifras máximo. 
+							SUMA=NUM1+NUM2; //Se realiza la suma. 
+							printf("ok NUM1:%d  NUM2:%d SUM:%d %s",NUM1,NUM2,SUMA,CRLF);//Imrpimo por pantalla el resultado de la suma y los dos números introducidos. 
+							sprintf_s(buffer_out, sizeof(buffer_out), "ok %d %s",SUMA,CRLF);//Envío al cliente el resultado de la suma y se lo imprimo por pantalla de forma segura. 
 						}else
 						sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
 					}
 					break;
 
 				default:
-					break;
+					    break;
 					
 			} // switch
 
@@ -254,17 +310,14 @@ main()
 						{
 							printf("CLIENTE> Conexión con el cliente cerrada\r\n");
 							estado=S_USER;
-							fin_conexion = 1;}
-						
-					
-		}
-			
-
+							fin_conexion = 1;
+						   }
+						}
 
 		} while (!fin_conexion);
 		printf ("SERVIDOR> CERRANDO CONEXION DE TRANSPORTE\r\n");
 		shutdown(nuevosockfd,SD_SEND);
-		closesocket(nuevosockfd);
+		closesocket(nuevosockfd); //Cierro el socket una vez se termina la comunicación. 
 
 	}while(!fin);
 
